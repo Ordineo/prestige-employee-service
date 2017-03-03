87e984324d94da88cd6988b7c6a4915a8d58b0b6
Fux <Sammi.Fux@ordina.be>
2017-03-03 14:43:29 +0100

    comment error code

--- src/main/java/be/ordina/prestige/repo/PrestigeRepository.java
+++ src/main/java/be/ordina/prestige/repo/PrestigeRepository.java
@@ -10,5 +10,5 @@ import java.util.Collection;
  * Created by SaFu on 1/03/2017.
  */
 public interface PrestigeRepository extends CrudRepository<Prestige, Integer> {
-    Collection<Prestige> findByCategory(@Param("categoryName") int categoryId);
+//    Collection<Prestige> findByCategory(@Param("categoryName") int categoryId);
 }
